0f350642a0ed137373d79fe0a204bdcb4b398223
alex <jersin2304@gmail.com>
2020-07-20 20:37:25 -0500

    asdf

--- app/src/main/java/com/example/letsgo/util/ExtensionFunctions.kt
+++ app/src/main/java/com/example/letsgo/util/ExtensionFunctions.kt
@@ -10,18 +10,18 @@ import java.util.*
 import kotlin.math.*
 
 fun distancia(lat1:Double,lon1:Double,lat2:Double,lon2:Double): Double {
-    val r = 6371e3; // metres
+    val r = 6371e3 // metres
     val p1 = lat1 * Math.PI/180; // φ, λ in radians
-    val p2 = lat2 * Math.PI/180;
-    val a1 = (lat2-lat1) * Math.PI/180;
-    val a2 = (lon2-lon1) * Math.PI/180;
+    val p2 = lat2 * Math.PI/180
+    val a1 = (lat2-lat1) * Math.PI/180
+    val a2 = (lon2-lon1) * Math.PI/180
 
     val a = sin(a1/2) * sin(a1/2) +
             cos(p1) * cos(p2) *
-            sin(a2/2) * sin(a2/2);
-    val c = 2 * atan2(sqrt(a), sqrt(1-a));
+            sin(a2/2) * sin(a2/2)
+    val c = 2 * atan2(sqrt(a), sqrt(1-a))
 
-    return r * c; // in metres
+    return r * c// in metres
 }
 
 fun Date.toISOString(): String {
